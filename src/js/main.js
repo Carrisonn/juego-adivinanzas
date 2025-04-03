5a1478117d434596fe3ba3b81af41d06b112667f
Carlos <carlos97181034535@gmail.com>
2025-04-04 00:33:58 +0200

    added: function to read user value

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import { riddles } from "./riddles.js";
+import { $userLifes, $userScore, $riddle, $userAnswer, $containerGame, $form, userAnswerObj } from "./constants.js";
+
+window.addEventListener('load', () => $form.reset());
 
 document.addEventListener("DOMContentLoaded", () => {
   showUserStatsAndRiddle();
-  //checkAnswer();
+  $userAnswer.addEventListener('input', readValue);
+  $form.addEventListener('submit', checkAnswer);
 });
 
 
@@ -14,11 +18,49 @@ let positionRiddle = 0;
 
 // Functions
 function showUserStatsAndRiddle() {
-  document.querySelector('.span-user-lives').textContent = userLifes;
-  document.querySelector('.span-user-score').textContent = userScore;
-  document.querySelector('.riddle').textContent = riddles[positionRiddle].riddle;
+  $userLifes.textContent = userLifes;
+  $userScore.textContent = userScore;
+  $riddle.textContent = riddles[positionRiddle].riddle;
 }
 
-//function checkAnswer() {
+function readValue(event) {
+  userAnswerObj.userAnswer = event.target.value.trim().toLowerCase();
+}
+
+//function checkAnswer(event) {
+//  event.preventDefault();
 //
+//  const { userAnswer } = userAnswerObj;
+//  if (userAnswer === '') return showNotification('El campo no puede estar vacío');
+//
+//  if (userAnswer !== riddles[positionRiddle].answer) {
+//    userLifes--;
+//    showNotification('Respuesta Incorrecta');
+//    showUserStatsAndRiddle();
+//    userAnswerObj.userAnswer = '';
+//    $form.reset();
+//    return;
+//  }
+//
+//  if (userAnswer === riddles[positionRiddle].answer) {
+//    userScore += 5;
+//    //if (positionRiddle === riddles.length)
+//    positionRiddle++;
+//    showUserStatsAndRiddle();
+//    userAnswerObj.userAnswer = '';
+//    $form.reset();
+//  }
+//}
+//
+//
+//function showNotification(message) {
+//  const notification = document.querySelector('.notification');
+//
+//  if (!notification) {
+//    const notification = document.createElement('p');
+//    notification.classList.add('notification');
+//    notification.textContent = message;
+//    $containerGame.appendChild(notification);
+//    setTimeout(() => notification.remove(), 3000);
+//  }
 //}
