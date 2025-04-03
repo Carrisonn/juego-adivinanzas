00f0a2996c2d8bc6537f822db31045b528628fac
Carlos <carlos97181034535@gmail.com>
2025-04-04 00:23:18 +0200

    added: an object and necessary html elements

---
+++ src/js/constants.js
@@ -0,0 +1,8 @@
+export const $userLifes = document.querySelector('.span-user-lives');
+export const $userScore = document.querySelector('.span-user-score');
+export const $riddle = document.querySelector('.riddle');
+export const $userAnswer = document.querySelector('#input-user-answer');
+export const $containerGame = document.querySelector('.container-game');
+export const $form = document.querySelector('#form');
+
+export const userAnswerObj = { userAnswer: '' };
