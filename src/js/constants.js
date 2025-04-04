fb570ddb918dfcb3e9f91bc7778f1771e061c2a2
Carlos <carlos97181034535@gmail.com>
2025-04-04 19:47:39 +0200

    change: constant renamed

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,7 +1,7 @@
 export const $userLifes = document.querySelector('.span-user-lives');
 export const $userScore = document.querySelector('.span-user-score');
 export const $riddle = document.querySelector('.riddle');
-export const $userAnswer = document.querySelector('#input-user-answer');
+export const $userInput = document.querySelector('#input-user-answer');
 export const $containerGame = document.querySelector('.container-game');
 export const $form = document.querySelector('#form');
 
