a9d4c2e25521c72acc3d2357a57172bfe41d94f0
Carlos <carlos97181034535@gmail.com>
2025-04-04 22:28:52 +0200

    added: function to manage the end of the game

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,24 @@
 import { riddles } from "./riddles.js";
-import { $userLifes, $userScore, $riddle, $userAnswer, $containerGame, $form, userAnswerObj } from "./constants.js";
+import { $userLifes, $userScore, $riddle, $userInput, $containerGame, $form, userAnswerObj } from "./constants.js";
 
 window.addEventListener('load', () => $form.reset());
 
 document.addEventListener("DOMContentLoaded", () => {
   showUserStatsAndRiddle();
-  $userAnswer.addEventListener('input', readValue);
+  $userInput.addEventListener('input', readValue);
   $form.addEventListener('submit', checkAnswer);
 });
 
 
 // States
-let userLifes = 5;
+let userLives = 5;
 let userScore = 0;
-let positionRiddle = 0;
+let positionRiddle = 0; // position of the riddle in the array
 
 
 // Functions
 function showUserStatsAndRiddle() {
-  $userLifes.textContent = userLifes;
+  $userLifes.textContent = userLives;
   $userScore.textContent = userScore;
   $riddle.textContent = riddles[positionRiddle].riddle;
 }
@@ -34,7 +34,7 @@ function checkAnswer(event) {
   if (userAnswer === '') return showNotification('El campo no puede estar vacío');
 
   if (userAnswer !== riddles[positionRiddle].answer) {
-    userLifes--;
+    userLives--;
     showNotification('Respuesta Incorrecta');
     showUserStatsAndRiddle();
     userAnswerObj.userAnswer = '';
@@ -43,8 +43,8 @@ function checkAnswer(event) {
   }
 
   if (userAnswer === riddles[positionRiddle].answer) {
+    if (positionRiddle + 1 === riddles.length) return gameFinished();
     userScore += 5;
-    //if (positionRiddle === riddles.length)
     positionRiddle++;
     showUserStatsAndRiddle();
     userAnswerObj.userAnswer = '';
@@ -52,6 +52,20 @@ function checkAnswer(event) {
   }
 }
 
+function gameFinished() {
+  userScore += 5;
+  $containerGame.innerHTML = `
+    <div class="game-finished-container">
+      <h1 class="win-title">¡Felicidades!</h1>
+      <p>Has adivinado todas las respuestas, a continuación se te muestran tus estadísticas</p>
+      <div class="user-stats stats-finished">
+        <p class="p-user-score">Puntos Totales: ${userScore}</p>
+        <p class="p-user-lives">Vidas Restantes: ${userLives}</p>
+      </div>
+      <button class="btn btn-restart" onclick="window.location.reload()">Volver a jugar</button>
+    </div>
+  `;
+}
 
 function showNotification(message) {
   const notification = document.querySelector('.notification');
