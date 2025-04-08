056993e87c3e2ef66de33299d710ccff8724ba8a
Carlos <carlos97181034535@gmail.com>
2025-04-08 19:17:02 +0200

    added: select the span of the tries in the html

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,6 @@
 export const $userLifes = document.querySelector('.span-user-lives');
 export const $userScore = document.querySelector('.span-user-score');
+export const $userTries = document.querySelector('.span-user-tries');
 export const $riddle = document.querySelector('.riddle');
 export const $userInput = document.querySelector('#input-user-answer');
 export const $containerGame = document.querySelector('.container-game');
